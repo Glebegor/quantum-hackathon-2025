2cde91845b7bff5160fb414cf2b32ee2e512c8e3
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2025-06-19 13:08:13 +0200

    MVC: Added anumations, templates and other shit.

--- mvc/mvc/settings.py
+++ mvc/mvc/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "misconcepts"
+    'misconcepts',
 ]
 
 MIDDLEWARE = [
@@ -115,10 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+
+# No STATICFILES_DIRS needed
+# STATICFILES_DIRS = [ ... ]  ← REMOVE this if using only per-app static
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
